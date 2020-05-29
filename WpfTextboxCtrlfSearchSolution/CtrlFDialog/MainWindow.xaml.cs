31c3bd6b327e269a501bd4ff6d991eb91bccc0cb
Edwin <edwinbronyusa@gmail.com>
2020-05-29 13:30:44 -0400

    first implementation, need serious code review. Not worrying about usercontrol, mvvm for now

---
+++ WpfTextboxCtrlfSearchSolution/CtrlFDialog/MainWindow.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace CtrlFDialog
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window, IDialogSearcheable
+    {
+        private bool _isInSearchMode;
+        private string _searchTerm;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MainWindow_OnPreviewKeyDown(object sender, KeyEventArgs e)
+        {
+            _isInSearchMode = _isInSearchMode && e.Key == Key.F3;
+
+            if (_isInSearchMode)
+            {
+                PerformSearch(_searchTerm);
+            }
+
+            if ((Keyboard.Modifiers & ModifierKeys.Control) == ModifierKeys.Control && e.Key == Key.F)
+            {
+                SearchDialog searchDialog = new SearchDialog(this);
+                searchDialog.Show();
+            }
+        }
+
+        public void PerformSearch(string content)
+        {
+            _isInSearchMode = true;
+            _searchTerm = content;
+
+            string originalContent = txtContent.Text.Substring(txtContent.CaretIndex + txtContent.SelectedText.Length);
+
+            int indexFound = originalContent.IndexOf(content, StringComparison.Ordinal);
+            if (indexFound == -1) return;
+
+            txtContent.Select(indexFound + (txtContent.Text.Length - originalContent.Length), content.Length);
+        }
+
+    }
+}
---
+++ WpfTextboxCtrlfSearchSolution/CtrlFDialog/SearchDialog.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace CtrlFDialog
+{
+    /// <summary>
+    /// Interaction logic for SearchDialog.xaml
+    /// </summary>
+    public partial class SearchDialog : Window
+    {
+        private readonly IDialogSearcheable _mainWindow;
+
+        public SearchDialog(IDialogSearcheable mainWindow)
+        {
+            _mainWindow = mainWindow;
+            InitializeComponent();
+        }
+
+        private void SearchDialog_OnLoaded(object sender, RoutedEventArgs e)
+        {
+            txtSearch.Focus();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+            _mainWindow.PerformSearch(txtSearch.Text.Trim());
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            this.Close();
+        }
+    }
+}
